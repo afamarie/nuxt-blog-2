210025352fe6b70e3b1ee4f51718aee3de718b85
afamaria <marie.afanasyeva@gmail.com>
2025-04-28 13:07:10 +0500

    feat: add pagination

---
+++ components/PostsPagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <UPagination
+    v-model:page="page"
+    :total="total"
+    :items-per-page="itemsPerPage"
+    @update:page="$emit('update:page', $event)"
+  >
+    <template #first>
+      <span />
+    </template>
+    <template #last>
+      <span />
+    </template>
+    <template
+      v-if="page === 1"
+      #prev
+    >
+      <span />
+    </template>
+    <template
+      v-if="page*itemsPerPage >= total"
+      #next
+    >
+      <span />
+    </template>
+  </UPagination>
+</template>
+
+<script setup lang="ts">
+const page = defineModel<number>({
+  default: 1,
+  type: Number,
+})
+
+defineProps<({
+  total: number
+  itemsPerPage: number
+})>()
+
+defineEmits<{
+  (e: 'update:page', page: number): void
+}>()
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="index-page">
+    <h1>Articles</h1>
+    <ul
+      ref="postsListRef"
+      class="posts-list"
+    >
+      <li
+        v-for="post in paginatedPosts"
+        :key="post.id"
+      >
+        <PostCard
+          :post="post"
+        />
+      </li>
+    </ul>
+    <PostsPagination
+      ref="paginationRef"
+      v-model="page"
+      :items-per-page="itemsPerPage"
+      :total="total"
+      @update:page="handlePageUpdate"
+    />
+  </section>
+</template>
+
+<script setup lang="ts">
+import type { Post } from '~/types'
+
+const postsListRef = ref<HTMLUListElement | null>(null)
+const { data: posts } = await useLazyFetch<Post[]>('/api/posts/all')
+const page = ref<number>(useRoute().query.page ? Number(useRoute().query.page) : 1)
+const total = posts.value?.length || 0
+const itemsPerPage = 8
+
+const handlePageUpdate = () => {
+  useRouter().push({
+    query: {
+      page: page.value,
+    },
+  })
+
+  if (postsListRef.value) {
+    postsListRef.value.scrollIntoView({ behavior: 'smooth' })
+  }
+}
+
+const paginatedPosts = computed<Post[]>(() => {
+  const start = (page.value - 1) * itemsPerPage
+  const end = start + itemsPerPage
+  return posts.value?.slice(start, end) || []
+})
+</script>
